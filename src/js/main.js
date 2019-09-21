98ad40ab55c6c912f9ca6d33ccf94dddcfbd46e9
Kasia <katarzyna-godlewska1@wp.pl>
2019-09-22 00:02:32 +0200

    Feat: add querySelectors and eventListeners to button and text area

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,48 @@
-"use strict";
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//  window.addEventListener('load', function() {
+//    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//      // Registration was successful
+//      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//    }, function(err) {
+//      // registration failed :(
+//      console.log('ServiceWorker registration failed: ', err);
+//    });
+//  });
+//}
 
 // place your code below
 
 
-console.log(`Hello world!`);
+//console.log(`Hello world!`);
 
+const buttonLoad = document.querySelector('.button__load--js');
+console.log(buttonLoad)
+const buttonSave = document.querySelector('.button__save--js');
+
+buttonLoad.addEventListener('click', (e) => {
+  const textStringify = localStorage.getItem('textArea');
+  const newText = JSON.parse(textStringify);
+  document.querySelector('.textArea--js').value = newText;
+});
+
+buttonSave.addEventListener('click', (e) => {
+  const text = document.querySelector('.textArea--js').value;
+  const jsonText = JSON.stringify(text);
+  localStorage.setItem('textArea', jsonText);
+});
+
+//const buttons = document.querySelector('.button--js');
+
+const focusTextArea = document.querySelector('.focus--js');
+
+if (localStorage.getItem('focusTextArea')) {
+  focusTextArea.value = localStorage.getItem('focusTextArea');
+}
+
+focusTextArea.addEventListener('keyup', (e) => {
+  localStorage.setItem("focusTextArea", e.target.value);
+})
 
